2d742a866595e7c629516c25bede7bb289492a72
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-06-07 19:44:36 +0300

    Added db seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts
-  get 'posts/index'
-
-  get 'posts/show'
+  resources :posts, only: [:index, :show]
+  root to: "posts#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.delete_all
+
+Post.create(
+	id: 1,
+	title: "My very first post.",
+	published_at: Time.now - 1.day,
+	body:
+	%Q{### There Is Something You Should Know!
+
+	This is my very first post using Markdown!
+
+	How do you like it?
+	}
+)
+
+Post.create(
+	id: 2,
+	title: "My second post.",
+	published_at: Time.now,
+	body:
+	%Q{### My List of Things To Do:
+
+	Here's just a list of things:
+
+	* write more posts
+	* write even more posts
+	* write even more posts!
+	}
+)
